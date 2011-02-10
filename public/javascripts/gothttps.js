be5ec2b12b5ebbcfccba4b9cd579073a9d7ecd5a
Daniel Walmsley <goldsounds@gmail.com>
2011-02-10 14:48:19 -0500

    minor updates for EFF demo

--- public/javascripts/gothttps.js
+++ public/javascripts/gothttps.js
@@ -8,4 +8,38 @@ jQuery(document).ready(function()
       jQuery(this).next('tr').toggle();
       jQuery(this).find('.arrow').toggleClass('up');
   }); 
+  
+  //add helper popups
+  
+  jQuery('.bubblehelp').each(function(e){
+    var element = jQuery(this);
+    var help_id = element.attr('id')+'_info';
+    var popup = jQuery('#'+help_id)
+    popup.addClass('bubble');
+  });
+  
+  jQuery('.bubble').hide();
+  
+  
+  
+  jQuery('.bubblehelp').append('<img src="/images/help-icon.png"/>').click(function(e){
+    var element = jQuery(this);
+    var help_id = element.attr('id')+'_info';
+    var popup = jQuery('#'+help_id);
+    
+    popup.slideToggle();
+    
+    var popup_height = popup.height();
+    
+    var pos = element.position();
+    
+    popup.css({ position: "absolute",
+            marginLeft: 0, marginTop: 0,
+            top: pos.top+60, left: pos.left });
+    
+    popup.click(function(e){
+      jQuery(this).hide();
+    });
+  });
 });
+
