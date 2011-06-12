6c2d94c9afa8b9a26753f101326037afa3e2b863
Daniel Walmsley <goldsounds@gmail.com>
2011-06-12 16:26:50 -0400

    cookies broken - WHYYYY?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Wedding::Application.config.session_store :cookie_store, :key => '_wedding_session', :secure => Rails.env.production?, :http_only => true
+Wedding::Application.config.session_store :cookie_store, :key => '_wedding_session', :secure => false, :http_only => true
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
